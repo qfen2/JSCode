941c833a39bef06d3f9bd1eb98d4030944ddfc7e
qfen2 <qfen_liu@163.com>
2021-12-02 02:52:40 +0800

    运算符

--- day03/demo1_运算符.js
+++ day03/demo1_运算符.js
@@ -1,5 +1,19 @@
 /*
     运算符也叫操作符
+
+    任何值和字符串做加法运算，都会先转化为字符串在进行运算
+    a=a+''; //隐式类型转换
+
+    任何值做- * /运算时都会自动转换成Number类型
+
+    一元运算符：只需要一个操作数
+    a =+ a
 */
-var a=null;
-console.log(typeof a);
+// var result;
+// result=100-'a';
+// console.log(result);
+// console.log(typeof result);  String
+
+var a=2;
+a+=a;
+console.log(a);
